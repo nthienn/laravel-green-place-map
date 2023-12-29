c1ff6279c0140a02fe176b076fb36a0886aa297a
Thiện Nguyễn <thiennguyen1701@gmail.com>
2023-12-29 16:57:44 +0700

    update routes

--- routes/web.php
+++ routes/web.php
@@ -21,43 +21,58 @@ use App\Http\Controllers\PlaceController;
 */
 
 // home
-Route::get('/', [HomeController::class, 'home']);
-Route::get('/places-outstanding', [HomeController::class, 'show_places'])->name('places-outstanding');
-Route::get('/places-type/{name}/{id}', [HomeController::class, 'show_places_type'])->name('places-type');
-Route::get('/search', [HomeController::class, 'search'])->name('search');
-Route::get('/place-detail/{id}', [HomeController::class, 'show_place_detail'])->name('place-detail');
+Route::controller(HomeController::class)->group(function () {
+    Route::get('/', 'home');
+    Route::get('/places-outstanding', 'show_places')->name('places-outstanding');
+    Route::get('/places-type/{name}/{id}', 'show_places_type')->name('places-type');
+    Route::get('/search', 'search')->name('search');
+    Route::get('/place-detail/{id}', 'show_place_detail')->name('place-detail');
+});
 
 // user
-Route::resource('/user', UserController::class);
-Route::post('/login', [UserController::class, 'login'])->name('login');
-Route::get('/logout', [UserController::class, 'logout'])->name('logout');
-Route::get('/change_password', [UserController::class, 'change_password'])->name('change_password');
-Route::put('/update_password', [UserController::class, 'update_password'])->name('update_password');
-Route::post('/forgot_password', [UserController::class, 'forgot_password'])->name('forgot_password');
+Route::controller(UserController::class)->group(function () {
+    Route::resource('/user');
+    Route::post('/login', 'login')->name('login');
+    Route::get('/logout', 'logout')->name('logout');
+    Route::get('/change_password', 'change_password')->name('change_password');
+    Route::put('/update_password', 'update_password')->name('update_password');
+    Route::post('/forgot_password', 'forgot_password')->name('forgot_password');
+});
 
 // place
 Route::resource('/place', PlaceController::class);
 Route::post('/evaluate/{id}', [EvaluateController::class, 'evaluate'])->name('evaluate');
 
 // admin routes
-Route::get('/admin', [AdminController::class, 'home']);
-Route::post('/admin-login', [AdminController::class, 'login'])->name('admin-login');
-Route::get('/admin-logout', [AdminController::class, 'logout'])->name('admin-logout');
+Route::controller(AdminController::class)->group(function () {
+    Route::get('/admin', 'home');
+    Route::post('/admin-login', 'login')->name('admin-login');
+    Route::get('/admin-logout', 'logout')->name('admin-logout');
+});
 
-Route::get('/places', [AdminPlaceController::class, 'show_places'])->name('places');
-Route::get('/place_detail/{id}', [AdminPlaceController::class, 'show_place_detail'])->name('place_detail');
-Route::get('/delete_place/{id}', [AdminPlaceController::class, 'delete_place'])->name('delete_place');
+// admin places
+Route::controller(AdminPlaceController::class)->group(function () {
+    Route::get('/places', 'show_places')->name('places');
+    Route::get('/place_detail/{id}', 'show_place_detail')->name('place_detail');
+    Route::get('/delete_place/{id}', 'delete_place')->name('delete_place');
 
-Route::get('/approves', [AdminPlaceController::class, 'show_approves'])->name('approves');
-Route::get('/approve_detail/{id}', [AdminPlaceController::class, 'show_approve_detail'])->name('approve_detail');
-Route::put('/approve/{id}', [AdminPlaceController::class, 'approve_place'])->name('approve');
-Route::get('/delete_approve/{id}', [AdminPlaceController::class, 'delete_approve'])->name('delete_approve');
+    Route::get('/approves', 'show_approves')->name('approves');
+    Route::get('/approve_detail/{id}', 'show_approve_detail')->name('approve_detail');
+    Route::put('/approve/{id}', 'approve_place')->name('approve');
+    Route::get('/delete_approve/{id}', 'delete_approve')->name('delete_approve');
+});
 
-Route::get('/suppliers', [AdminUserController::class, 'show_suppliers'])->name('suppliers');
-Route::get('/members', [AdminUserController::class, 'show_members'])->name('members');
-Route::put('/authorize_supplier/{id}', [AdminUserController::class, 'authorize_supplier'])->name('authorize_supplier');
-Route::get('/delete_member/{id}', [AdminUserController::class, 'delete_member'])->name('delete_member');
+// admin users
+Route::controller(AdminUserController::class)->group(function () {
+    Route::get('/suppliers', 'show_suppliers')->name('suppliers');
+    Route::get('/members', 'show_members')->name('members');
+    Route::put('/authorize_supplier/{id}', 'authorize_supplier')->name('authorize_supplier');
+    Route::get('/delete_member/{id}', 'delete_member')->name('delete_member');
+});
 
-Route::get('/comments', [EvaluateController::class, 'show_comments'])->name('comments');
-Route::put('/hide_comment/{id}', [EvaluateController::class, 'hide_comment'])->name('hide_comment');
-Route::put('/show_comment/{id}', [EvaluateController::class, 'show_comment'])->name('show_comment');
+// admin comments
+Route::controller(EvaluateController::class)->group(function () {
+    Route::get('/comments', 'show_comments')->name('comments');
+    Route::put('/hide_comment/{id}', 'hide_comment')->name('hide_comment');
+    Route::put('/show_comment/{id}', 'show_comment')->name('show_comment');
+});
